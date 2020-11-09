4c8c6d6f52755469a3b7e2b6eebe35a1fc46684b
joe12801 <70465495+joe12801@users.noreply.github.com>
2020-11-10 05:20:25 +0800

    ffmpeg 自动生成字幕

---
+++ path.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+files=$3
+#获取扩展名
+#ext=${files##*.}
+#fl=$(echo $files|sed -e 's/ /\\ /g;s/\[/\\[/g;s/\]/\\]/g;s/(/\\(/g; s/)/\\)/g')
+spath=${files%/*}
+##字幕特殊
+ss=${files%.*}
+fl=$(echo $ss|sed -e 's/\[/\\[/g;s/\]/\\]/g')
+nspath="${spath}_en.mp4"
+srt="${fl}.srt"
+#srt="Simplified\ Chinese.chi.srt"
+# ffmpeg  -i "$files"  -threads 200 -preset ultrafast  -vf subtitles="$srt" "$nspath" 
+nohup ffmpeg  -i "$files"  -threads 200 -preset ultrafast  -vf subtitles="$srt" "$nspath" >/dev/null 2>&1 &
+str="\n"
+sstr=$(echo -e $str)
+echo $sstr

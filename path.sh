29ca68fa556315b083a6244f4ebc4ea86e3be5b1
joe12801 <70465495+joe12801@users.noreply.github.com>
2020-11-12 02:21:33 +0800

    Update path.sh

--- path.sh
+++ path.sh
@@ -21,7 +21,8 @@ echo $srt
 #srt="${fl}.srt"
 #srt="Simplified\ Chinese.chi.srt"
 # ffmpeg  -i "$files"  -threads 200 -preset ultrafast  -vf subtitles="$srt" "$nspath" 
-nohup ffmpeg  -i "$files"  -threads 200 -preset ultrafast  -vf subtitles="$srt" "$nspath" >/dev/null 2>&1 &
+#nohup ffmpeg  -i "$files"  -threads 200 -preset ultrafast  -vf subtitles="$srt" "$nspath" >/dev/null 2>&1 &
+nohup ffmpeg  -i "$files"  -threads 200 -preset ultrafast  -vf subtitles="$srt" "$nspath" 
 str="\n"
 sstr=$(echo -e $str)
 echo $sstr

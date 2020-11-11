d9637bac2d28ae02d95b0fff5f92302f2a7762e4
joe12801 <70465495+joe12801@users.noreply.github.com>
2020-11-12 02:02:15 +0800

    Update path.sh

--- path.sh
+++ path.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
-
+  
 files=$3
 #获取扩展名
 #ext=${files##*.}
 #fl=$(echo $files|sed -e 's/ /\\ /g;s/\[/\\[/g;s/\]/\\]/g;s/(/\\(/g; s/)/\\)/g')
+main_path=${files%/*}
+echo $main_path
 spath=${files%/*}
+last=${files##*/}
+echo $last
 ##字幕特殊
 ss=${files%.*}
+#fl=$(echo $main_path|sed -e 's/\[/\\[/g;s/\]/\\]/g')
 fl=$(echo $ss|sed -e 's/ /\\ /g;s/\[/\\[/g;s/\]/\\]/g;s/(/\\(/g; s/)/\\)/g')
-#fl=$(echo $ss|sed -e 's/\[/\\[/g;s/\]/\\]/g')
+last_srt=$(echo $last|sed -e 's/\[/\\[/g;s/\]/\\]/g')
 nspath="${spath}_en.mp4"
 srt="${fl}.srt"
+echo $srt
+#srt="${fl}.srt"
 #srt="Simplified\ Chinese.chi.srt"
 # ffmpeg  -i "$files"  -threads 200 -preset ultrafast  -vf subtitles="$srt" "$nspath" 
 nohup ffmpeg  -i "$files"  -threads 200 -preset ultrafast  -vf subtitles="$srt" "$nspath" >/dev/null 2>&1 &

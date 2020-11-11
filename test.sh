c5448cde47f289963330e94cb31717af942cc8a0
joe12801 <70465495+joe12801@users.noreply.github.com>
2020-11-12 03:21:58 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-  
+echo 33 >> 33.txt
+echo $3 >> 33.txt
 files="/content/drive/Shared drives/Happy English Drive 2/google cola/The SpongeBob Movie Sponge On The Run (2020) [1080p] [WEBRip] [5.1] [YTS.MX]/The.SpongeBob.Movie.Sponge.On.The.Run.2020.1080p.WEBRip.x264.AAC5.1-[YTS.MX].mp4"
 #获取扩展名
 #ext=${files##*.}

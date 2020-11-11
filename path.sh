c5fd4b495596de27d23819d266e33b13f197e603
joe12801 <70465495+joe12801@users.noreply.github.com>
2020-11-12 01:18:49 +0800

    Update path.sh

--- path.sh
+++ path.sh
@@ -7,12 +7,13 @@ files=$3
 spath=${files%/*}
 ##字幕特殊
 ss=${files%.*}
-fl=$(echo $ss|sed -e 's/\[/\\[/g;s/\]/\\]/g')
+fl=$(echo $ss|sed -e 's/ /\\ /g;s/\[/\\[/g;s/\]/\\]/g;s/(/\\(/g; s/)/\\)/g')
+#fl=$(echo $ss|sed -e 's/\[/\\[/g;s/\]/\\]/g')
 nspath="${spath}_en.mp4"
 srt="${fl}.srt"
 #srt="Simplified\ Chinese.chi.srt"
 # ffmpeg  -i "$files"  -threads 200 -preset ultrafast  -vf subtitles="$srt" "$nspath" 
-ffmpeg  -i "$files"  -threads 200 -preset ultrafast  -vf subtitles="$srt" "$nspath" >/dev/null 2>&1 &
+nohup ffmpeg  -i "$files"  -threads 200 -preset ultrafast  -vf subtitles="$srt" "$nspath" >/dev/null 2>&1 &
 str="\n"
 sstr=$(echo -e $str)
 echo $sstr

fba4899b371a4e52fe1fa82f7aff28eae5e031a5
joe12801 <70465495+joe12801@users.noreply.github.com>
2021-01-18 13:43:27 +0800

    Update ctmd5.sh

--- ctmd5.sh
+++ ctmd5.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-echo $3 >> 3.txt
 /usr/local/bin/ctmd5 $3
 year=`date +%Y`
 month=`date +%m`
 day=`date +%d`
 filepath="$year$month$day"
-/usr/local/share/BaiduPCS-Go-v3.6.2-linux-arm64/BaiduPCS-Go mkdir $filepath
+/root/BaiduPCS-Go-v3.6.2-linux-amd64/BaiduPCS-Go mkdir $filepath
 str=$"\n"
-nohup /usr/local/share/BaiduPCS-Go-v3.6.2-linux-arm64/BaiduPCS-Go u $3 /$filepath  >/dev/null 2>&1 &
+nohup /root/BaiduPCS-Go-v3.6.2-linux-amd64/BaiduPCS-Go u $3 /$filepath  >/dev/null 2>&1 &
 sstr=$(echo -e $str)
 echo $sstr
 
